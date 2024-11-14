b74d3bb0988ee36ccab205d0d26391b36087510b
sahildogra18 <sahildgr18@gmail.com>
2024-11-15 00:18:52 +0530

    Resolved merge conflicts

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,16 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./assets/Components/Navbar";
-import Herosection from "./assets/Components/Herosection";
+
 import Menu from "./pages/Menu";
 import Location from "./pages/Location";
+import Home from "./assets/Components/Home";
 
 function App() {
-<<<<<<< HEAD
-=======
-
->>>>>>> 290fc221b97ad6c81f24a0d57e5593f11797bbef
   return (
     <>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<Navbar />} />
-        <Route path="/" element={<Herosection />} />
-=======
->>>>>>> 290fc221b97ad6c81f24a0d57e5593f11797bbef
+        <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/location" element={<Location />} />
       </Routes>
---
+++ my-react-app/src/assets/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Herosection from "./Herosection";
+import Navbar from "./Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Herosection />
+    </>
+  );
+}
+
+export default Home;
